a614695cd998f0bdc2f0e3c6816c4953c58f151f
ahmed-mohamed-77 <ahm_ed5722@hotmail.com>
2024-05-27 18:58:10 +0300

    print multiple car usinf diff classes

--- oop_practice.py
+++ oop_practice.py
@@ -3,22 +3,56 @@ import random
 import os
 
 # place Pygame window at a specific location
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50,50)
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50, 50)
+
+class Vehicle:
+    def __init__(self, color: str = "blue", x: int = 400, y: int = 400) -> None:
+        self.img_path: str = "sedan_" + color + ".png"
+        self.location: tuple[int, int] = (x, y)
+        self.draw()
+
+    def draw(self):
+        # load image and set its location
+        self.img = pygame.image.load(self.img_path)
+        self.img_location = self.img.get_rect(center=self.location)
+
+class Truck(Vehicle):
+    def __init__(self, x, y, kind="truck_tractor") -> None:
+        self.kind = kind
+        super().__init__(color="green", x=x, y=y)
+        self.img_path = self.kind + ".png"
+        self.draw()
+
+class Police(Vehicle):
+    def __init__(self, x, y) -> None:
+        super().__init__(color="green", x=x, y=y)
+        self.img_path = "police_car.png"
+        self.draw()
 
 # pygame settings
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 running = True
 
-# load image and set its location
-img = pygame.image.load("sedan_red.png")
-img_location = img.get_rect()
-img_location.center = 400, 400
+cars = []
+for i in range(10):
+    color = random.choice(["blue", "red", "green"])
+    x = random.randint(0, 800)
+    y = random.randint(0, 800)
+    car_class = random.choice(["sedan", "truck", "police"])
+    if car_class == "sedan":
+        cars.append(Vehicle(color, x, y))
+    elif car_class == "truck":
+        truck_kind = random.choice(["truck_tractor", "box_truck"])
+        cars.append(Truck(x, y, truck_kind))
+    elif car_class == "police":
+        cars.append(Police(x, y))
 
 # set background color
 screen.fill("white")
 # place image on the screen
-screen.blit(img, img_location)
+for car in cars:
+    screen.blit(car.img, car.img_location)
 
 # start game loop
 while running:
@@ -31,4 +65,3 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
-
